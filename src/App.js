a6aca4b34494feccff94d225715613d09c064203
Esther Okiki <estherokiki15@gmail.com>
2024-12-19 04:53:01 +0100

    add more components

--- src/App.js
+++ src/App.js
@@ -23,6 +23,33 @@ const links = [
   },
 ];
 
+const mainCards = [
+  {
+    Img: "/img/female-yoga.png",
+    Heading: "Flex Muscle",
+    Text: "Lorem Ipsum.....",
+    Id: 2,
+  },
+  {
+    Img: "/img/male-yoga.png",
+    Heading: "Cardio Exercise",
+    Text: "Lorem Ipsum.....",
+    Id: 11,
+  },
+  {
+    Img: "/img/female-yoga.png",
+    Heading: "Basic Yoga",
+    Text: "Lorem Ipsum.....",
+    Id: 9,
+  },
+  {
+    Img: "/img/male-yoga.png",
+    Heading: "Weight Lifting",
+    Text: "Lorem Ipsum.....",
+    Id: 27,
+  },
+];
+
 export default function App() {
   const [btnActive, setBtnActive] = useState(false);
 
@@ -30,67 +57,11 @@ export default function App() {
     <div>
       <header className="header">
         <NavBar links={links} />
-
-        <section className="hero">
-          <div className="hero-texts">
-            <div className="hero-texts--text">
-              <h1 className="hero-texts--heading">Transform</h1>
-              <h2 className="orange-accent hero-texts--heading">Your Body</h2>
-
-              <p class="hero-texts--subtexts">
-                Join us to sculpt your dream physique and embrace a healthier,
-                more vibrant life!
-              </p>
-            </div>
-
-            <button
-              className={
-                btnActive
-                  ? "hero__texts-btn hero-btn-active"
-                  : "hero__texts-btn b-active"
-              }
-              onMouseEnter={() => setBtnActive(true)}
-              onMouseLeave={() => setBtnActive(false)}
-            >
-              <a href="#">
-                Get Moving
-                <span
-                  className={
-                    btnActive
-                      ? "arr-right arr-right-active "
-                      : "arr-right b-active"
-                  }
-                >
-                  &rarr;
-                </span>
-              </a>
-            </button>
-          </div>
-
-          <div className="hero__imgs">
-            <div className="hero__imgs-img hero__imgs-img-female">
-              <img
-                src="./img/landingPage-female.png"
-                alt="gym girl"
-                width="300"
-                height="300"
-              />
-            </div>
-
-            <div className="hero__imgs-img hero__imgs-img-male">
-              <img
-                src="./img/landingPage-male.png"
-                alt="gym boy"
-                width="300"
-                height="300"
-              />
-            </div>
-          </div>
-        </section>
-        <div className="hero__triangle"></div>
+        <Hero btnActive={btnActive} setBtnActive={setBtnActive} />
+        <HeroTriangle />
       </header>
 
-      <main className="main"></main>
+      <Main main={mainCards} />
     </div>
   );
 }
@@ -208,6 +179,148 @@ function NavBarLinks({ links }) {
           </li>
         ))}
       </ul>
+    </div>
+  );
+}
+
+function Hero({ btnActive, setBtnActive }) {
+  return (
+    <section className="hero">
+      <HeroTexts btnActive={btnActive} setBtnActive={setBtnActive} />
+      <HeroImgs />
+    </section>
+  );
+}
+
+function HeroTriangle() {
+  return <div className="hero__triangle"></div>;
+}
+
+function HeroTexts({ btnActive, setBtnActive }) {
+  return (
+    <div className="hero-texts">
+      <div className="hero-texts--text">
+        <h1 className="hero-texts--heading">Transform</h1>
+        <h2 className="orange-accent hero-texts--heading">Your Body</h2>
+
+        <p className="hero-texts--subtexts">
+          Join us to sculpt your dream physique and embrace a healthier, more
+          vibrant life!
+        </p>
+      </div>
+
+      <button
+        className={
+          btnActive
+            ? "hero__texts-btn hero-btn-active"
+            : "hero__texts-btn b-active"
+        }
+        onMouseEnter={() => setBtnActive(true)}
+        onMouseLeave={() => setBtnActive(false)}
+      >
+        <a href="#">
+          Get Moving
+          <span
+            className={
+              btnActive ? "arr-right arr-right-active " : "arr-right b-active"
+            }
+          >
+            &rarr;
+          </span>
+        </a>
+      </button>
+    </div>
+  );
+}
+
+function HeroImgs() {
+  return (
+    <div className="hero__imgs">
+      <div className="hero__imgs-img hero__imgs-img-female">
+        <img
+          src="./img/landingPage-female.png"
+          alt="gym girl"
+          width="300"
+          height="300"
+        />
+      </div>
+
+      <div className="hero__imgs-img hero__imgs-img-male">
+        <img
+          src="./img/landingPage-male.png"
+          alt="gym boy"
+          width="300"
+          height="300"
+        />
+      </div>
+    </div>
+  );
+}
+
+function Main({ main }) {
+  return (
+    <main className="main">
+      <MainBrandImgsImg />
+      <MainCards main={main} />
+    </main>
+  );
+}
+
+function MainBrandImgsImg() {
+  return (
+    <div className="main-brand-imgs">
+      <MainBrandImgs>
+        <img src="./img/logo4.png" width="100" height="100" alt="brandImg" />
+      </MainBrandImgs>
+      <MainBrandImgs>
+        <img src="./img/logo1.png" width="100" height="100" alt="brandImg" />
+      </MainBrandImgs>
+      <MainBrandImgs>
+        <img src="./img/logo2.png" width="100" height="100" alt="brandImg" />
+      </MainBrandImgs>
+      <MainBrandImgs>
+        <img src="./img/logo3.png" width="100" height="100" alt="brandImg" />
+      </MainBrandImgs>
+    </div>
+  );
+}
+
+function MainBrandImgs({ children }) {
+  return <div className="main-brand-imgs--img">{children}</div>;
+}
+
+function MainCards({ main }) {
+  return (
+    <div>
+      <MainCardsHeading />
+      <MainCardsCards main={main} />
+    </div>
+  );
+}
+
+function MainCardsHeading() {
+  return (
+    <div className="mainCards-heading">
+      <h2 className="mainCards-heading-heading">Our Program</h2>
+      <p className="mainCards-heading-subheading">
+        build your <span>dream body</span>
+      </p>
+    </div>
+  );
+}
+
+function MainCardsCards({ main }) {
+  return (
+    <div className="mainCards-cards">
+      {main.map((i) => (
+        <section key={i.Id} className="mainCards-cards--sect">
+          <figure className="mainCards-cards--img">
+            <img src={i.Img} alt={i.Img} width="50" height="50" />
+          </figure>
+          <h2 className="mainCards-cards--heading">{i.Heading}</h2>
+          <p className="mainCards-cards--texts">{i.Text}</p>
+        </section>
+      ))}
     </div>
   );
 }
